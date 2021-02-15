bd2086c4986700688f8b14a24e99200c15a9fa4f
Joao Barros <j.barros@ua.pt>
2021-02-15 14:41:07 +0000

    Pieces and Board

---
+++ Chess/board/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.board
+{
+    enum Color
+    {
+        White,
+        Black,
+        Red,
+        Pink,
+        Green
+    }
+}
---
+++ Chess/board/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.board
+{
+    class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int manyMovements { get; protected set; }
+        public Board chessboard { get; protected set; }
+
+        public Piece(Position position, Color color, Board chessboard)
+        {
+            this.position = position;
+            this.color = color;
+            this.chessboard = chessboard;
+            this.manyMovements = 0;
+        }
+    }
+}
